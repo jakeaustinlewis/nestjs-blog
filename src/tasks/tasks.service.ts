b1c55be3f9c47b84c9a6f156bf997737a65cd74f
Jake <jakelewis@Jakes-MBP.lan>
2022-03-27 18:24:29 -0500

    feat: create task endpoint

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  public createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid,
+      title,
+      description,
+      status: TaskStatus.Open,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
