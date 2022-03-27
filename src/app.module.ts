582749a0bc42708bd2489e8df653ed21ea93ce40
Jake <jakelewis@Jakes-MBP.lan>
2022-03-27 17:25:29 -0500

    feat: intial logic to retrieve all tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
+// Providers: Array of providers to be available within the module via dependancy injection
+
+// Controllers: Array of controllers to be instantiated within the module.
+
+// Exports: Array of providers to export to other modules.
+
+// Imports: List of modules required by this module. Any exported provider by
+// thtese modules will now be available in our module via dependancy injection
 @Module({
-  imports: [],
+  imports: [TasksModule],
 })
 export class AppModule {}
