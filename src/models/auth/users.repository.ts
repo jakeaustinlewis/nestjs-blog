4977c2fe0f94ba6944c5beb8ceef7252cb879f20
Jake <jakelewis@Jakes-MBP.lan>
2022-04-18 16:05:43 -0500

    feat: password salts

--- src/models/auth/users.repository.ts
+++ src/models/auth/users.repository.ts
@@ -3,6 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { UserDto } from './dtos/user.dto';
 import { User } from './entities/user.entity';
 import { UserMapper } from './mappers/user.mapper';
@@ -10,14 +11,18 @@ import { UserMapper } from './mappers/user.mapper';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(userDto: UserDto): Promise<void> {
-    const user = UserMapper.toEntity(userDto);
+    const { password } = userDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = UserMapper.toEntity({ ...userDto, password: hashedPassword });
 
     try {
       await this.save(user);
     } catch (error) {
       if (error.code === '23505') {
         // duplicate username
-
         throw new ConflictException('Email already exists');
       } else {
         throw new InternalServerErrorException();
